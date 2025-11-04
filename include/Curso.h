b927ee4715edce0c2a0407fb5572da2dad8fbd95
NoeliaUTN2025 <sueldonoe85@gmail.com>
2025-11-03 23:36:36 -0300

    Se agrega clase Curso

--- include/Curso.h
+++ include/Curso.h
@@ -1,6 +1,33 @@
-#ifndef CURSO_H_INCLUDED
-#define CURSO_H_INCLUDED
+
+#pragma once
+
+#include <cstring>
+#include <string>
+
+class Curso {
+private:
+    int _idCurso;
+    int _legajoDocente;
+    char _nombreCurso [100];
+    int _numeroAula;
+    bool _eliminado;
+
+public:
+    Curso ();
+    Curso(int idCurso, int legajoDocente,  const char nombreCurso [], int numeroAula, bool eliminado);
+
+    int getIdCurso () const;
+    int getLegajoDocente() const;
+    std::string  getNombreCurso () const;
+    int getNumeroAula() const;
+    bool getEliminado() const;
 
 
+    void setIdCurso (int idCurso);
+    void setLegajoDocente(int legajoDocente);
+    void setNombreCurso ( const std::string& nombreCurso);
+    void setNumeroAula(int numeroAula);
+    void setEliminado(bool eliminado);
 
-#endif // CURSO_H_INCLUDED
+};
+
--- src/Curso.cpp
+++ src/Curso.cpp
@@ -1,1 +1,67 @@
+#include "../include/Curso.h"
+
+using namespace std;
+
+Curso::Curso() {
+    _idCurso =  0;
+    _legajoDocente = 0;
+    strcpy (_nombreCurso ,   "");
+    _numeroAula = 0;
+    _eliminado = false;
+
+}
+
+Curso::Curso(int idCurso, int legajoDocente,  const char nombreCurso [], int numeroAula, bool eliminado ) {
+    _idCurso = idCurso;
+    _legajoDocente = legajoDocente;
+    strcpy (_nombreCurso, nombreCurso);
+    _numeroAula = numeroAula;
+    _eliminado = eliminado;
+
+}
+
+int Curso::getIdCurso()const{
+    return _idCurso;
+}
+
+int Curso::getLegajoDocente()const{
+    return _legajoDocente;
+}
+
+string  Curso::getNombreCurso()const{
+    return _nombreCurso;
+}
+
+int Curso::getNumeroAula()const{
+    return _numeroAula;
+}
+
+bool Curso::getEliminado()const{
+    return _eliminado;
+}
+
+/////////////////////////////////
+
+void Curso::setIdCurso(int idCurso){
+    _idCurso= idCurso;
+}
+
+void Curso::setLegajoDocente(int legajoDocente){
+    _legajoDocente = legajoDocente;
+}
+
+void  Curso::setNombreCurso( const std::string& nombreCurso){
+    strcpy (_nombreCurso,  nombreCurso.c_str());
+}
+
+void Curso::setNumeroAula (int numeroAula){
+    _numeroAula =   numeroAula;
+}
+
+void Curso::setEliminado(bool  eliminado){
+    _eliminado = eliminado;
+}
+
+
+
 
