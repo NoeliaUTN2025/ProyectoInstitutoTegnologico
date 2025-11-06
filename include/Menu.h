a674dcacef3b250f89304eca4bc2007679557566
Diego J D Arias <diegojdarias@gmail.com>
2025-11-06 19:12:37 -0300

    Se crea la clase Menu

---
+++ include/Menu.h
@@ -0,0 +1,24 @@
+/**
+ * @file Menu.h
+ * @brief Definición de la clase Menu para el manejo de todos los menus del sistema.
+ * @author Diego J D Arias, Noelia Sueldo.
+ * @date 2025-11-06
+ * @version 1.0
+ * @copyright MIT License
+ *
+ * Clase para representar y manipular los menus del sistema académico.
+ */
+
+
+#ifndef MENU_H
+#define MENU_H
+
+
+class Menu{
+public:
+    Menu() = default;
+
+    void start() const;
+};
+
+#endif // MENU_H
