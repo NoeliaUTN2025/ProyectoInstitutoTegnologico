d56ab3a27f55727fc2ed29bc38262dc0c82413b3
Diego J D Arias <diegojdarias@gmail.com>
2025-11-07 14:56:16 -0300

    Nueva clase GestorInscripcion para la gestion de Inscripciones, y actualizacion del proyecto

---
+++ include/GestorInscripcion.h
@@ -0,0 +1,24 @@
+/**
+ * @file GestorInscripcion.h
+ * @brief Definición de la clase GestorInscripcion para el sistema académico
+ * @author Diego J D Arias, Noelia Sueldo.
+ * @date 2025-11-07
+ * @version 1.0
+ * @copyright MIT License
+ */
+
+
+#ifndef GESTORINSCRIPCION_H
+#define GESTORINSCRIPCION_H
+
+#include "Inscripcion.h"
+#include "ArchivoManager.h"
+
+
+class GestorInscripcion {
+public:
+    GestorInscripcion();
+
+};
+
+#endif // GESTORINSCRIPCION_H
---
+++ src/GestorInscripcion.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file GestorInscripcion.cpp
+ * @brief Implementacion de la clase GestorInscripcion
+ * @author Diego J D Arias, Noelia Sueldo.
+ * @date 2025-11-02
+ * @version 1.0
+ * @copyright MIT License
+ */
+
+
+#include "GestorInscripcion.h"
+
+
+GestorInscripcion::GestorInscripcion() {
+}
