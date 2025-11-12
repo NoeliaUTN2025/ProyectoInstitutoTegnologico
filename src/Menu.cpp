28bcebe21a1f0d414fbde753558c153c97205098
Diego J D Arias <diegojdarias@gmail.com>
2025-11-12 19:01:43 -0300

    Se implementan los nuevos metodos, con opciones basicas

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -30,12 +30,12 @@ void Menu::start()const {
         std::cin >> option;
 
         switch (option) {
-            case 1: //menuAlumno(); break;
-            case 2: //menuDocente(); break;
-            case 3: //menuCurso(); break;
+            case 1: menuStudent(); break;
+            case 2: menuTeacher(); break;
+            case 3: menuCourse(); break;
             case 4: menuRegistration(); break;
-            case 5: //menuReporteInforme(); break;
-            case 6: //menuBackupRestauracion(); break;
+            case 5: menuReports(); break;
+            case 6: menuBackup(); break;
             case 0: std::cout << "Saliendo...\n"; break;
             default: std::cout << "Opcion invalida!\n"; system("pause");
         }
@@ -46,7 +46,7 @@ void Menu::menuRegistration()const {
     int option;
 
     std::string menuOptions[]{
-        "GESTION - INCRIPCION", "1. Nueva inscripción",
+        "GESTION DE INCRIPCIONES", "1. Nueva inscripción",
         "2. Buscar inscripciones por alumno", "3. Buscar inscripciones por docente",
         "4. Buscar inscripciones por curso", "5. Dar de baja inscripción",
         "0. Volver al menú principal" };
@@ -64,7 +64,144 @@ void Menu::menuRegistration()const {
             case 3:
             case 4:
             case 5:
+            case 0: break;
+            default: std::cout << "Opcion invalida!\n"; system("pause");
+        }
+    } while (option != 0);
+}
+
+void Menu::menuStudent()const {
+    int option;
+
+    std::string menuOptions[]{
+        "GESTIÓN DE ALUMNOS", "1. Alta de alumno",
+        "2. Listar alumnos", "3. Buscar alumno por legajo",
+        "4. Modificar datos de alumno", "5. Dar de baja alumno",
+        "6. Mostrar alumnos dados de baja", "0. Volver al menú principal" };
+
+        int arrayLength{ sizeof(menuOptions) / sizeof(std::string) };
+
+    do {
+        system("cls");
+        draw(menuOptions, arrayLength);
+        std::cin >> option;
+
+        switch (option) {
+            case 1: //@TODO: definir
+            case 2:
+            case 3:
+            case 4:
+            case 5:
             case 6:
+            case 0: break;
+            default: std::cout << "Opcion invalida!\n"; system("pause");
+        }
+    } while (option != 0);
+}
+
+void Menu::menuTeacher()const {
+    int option;
+
+    std::string menuOptions[]{
+        "GESTIÓN DE DOCENTES", "1. Alta de docente",
+        "2. Listar docente", "3. Buscar docente por legajo",
+        "4. Modificar datos de docente", "5. Dar de baja docente",
+        "6. Mostrar docentes dados de baja", "0. Volver al menú principal" };
+
+        int arrayLength{ sizeof(menuOptions) / sizeof(std::string) };
+
+    do {
+        system("cls");
+        draw(menuOptions, arrayLength);
+        std::cin >> option;
+
+        switch (option) {
+            case 1: //@TODO: definir
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 0: break;
+            default: std::cout << "Opcion invalida!\n"; system("pause");
+        }
+    } while (option != 0);
+}
+
+void Menu::menuCourse()const {
+    int option;
+
+    std::string menuOptions[]{
+        "GESTIÓN DE CURSOS", "1. Alta de curso",
+        "2. Listar cursos", "3. Buscar curso por id",
+        "4. Modificar datos de curso", "5. Dar de baja curso",
+        "6. Mostrar cursos dados de baja", "0. Volver al menú principal" };
+
+        int arrayLength{ sizeof(menuOptions) / sizeof(std::string) };
+
+    do {
+        system("cls");
+        draw(menuOptions, arrayLength);
+        std::cin >> option;
+
+        switch (option) {
+            case 1: //@TODO: definir
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 0: break;
+            default: std::cout << "Opcion invalida!\n"; system("pause");
+        }
+    } while (option != 0);
+}
+
+void Menu::menuReports()const {
+    int option;
+
+    std::string menuOptions[]{
+        "REPORTES Y CONSULTAS", "1. Listar alumnos por curso",
+        "2. Listar cursos por docente", "3. Listar inscripciones entre fechas",
+        "4. Alumnos con más inscripciones", "0. Volver al menú principal" };
+
+        int arrayLength{ sizeof(menuOptions) / sizeof(std::string) };
+
+    do {
+        system("cls");
+        draw(menuOptions, arrayLength);
+        std::cin >> option;
+
+        switch (option) {
+            case 1: //@TODO: definir
+            case 2:
+            case 3:
+            case 4:
+            case 0: break;
+            default: std::cout << "Opcion invalida!\n"; system("pause");
+        }
+    } while (option != 0);
+}
+
+void Menu::menuBackup()const {
+    int option;
+
+    std::string menuOptions[]{
+        "COPIAS DE SEGURIDAD Y BACKUP", "1. Backup de archivos (.dat)",
+        "2. Restaurar desde backup", "0. Volver al menú principal" };
+
+        int arrayLength{ sizeof(menuOptions) / sizeof(std::string) };
+
+    do {
+        system("cls");
+        draw(menuOptions, arrayLength);
+        std::cin >> option;
+
+        switch (option) {
+            case 1: //@TODO: definir
+            case 2:
+            case 3:
+            case 4:
             case 0: break;
             default: std::cout << "Opcion invalida!\n"; system("pause");
         }
